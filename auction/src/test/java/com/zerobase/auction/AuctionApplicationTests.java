96fb4fe5aef46c99f9a35a56a21ae47fafc54078
JoHyungJun <jhj8371@naver.com>
2023-07-14 23:14:35 +0900

    project init

---
+++ auction/src/test/java/com/zerobase/auction/AuctionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zerobase.auction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuctionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
